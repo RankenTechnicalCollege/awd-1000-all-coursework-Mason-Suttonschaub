a49dc892477026beafe0a7111824e3aa70cbdce5
Mason-Suttonschaub <Mason_Suttonschaub@insideranken.org>
2025-10-15 11:51:24 -0500

    lab1 & 2 for chapter 10

---
+++ HTML_CSS/Labs/HTML_CH10_Labs/Lab1/music/scripts/scripts.js
@@ -0,0 +1,15 @@
+/*
+Name: Mason_Sutton-Schaub
+Date: 10/15/2025
+File Name: scripts.js
+*/
+
+//Hamburger menu function
+function hamburger() {
+  var menu = document.getElementById("menu-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
