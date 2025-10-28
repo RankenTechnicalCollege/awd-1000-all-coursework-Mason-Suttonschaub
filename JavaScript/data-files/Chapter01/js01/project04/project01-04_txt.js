d7c4b8cfd4c017c8cea0b28e23b3574988538454
Mason-Suttonschaub <Mason_Suttonschaub@insideranken.org>
2025-10-28 11:51:56 -0500

    javascript project 1-3 and some of 4

--- JavaScript/data-files/Chapter01/js01/project04/project01-04_txt.js
+++ JavaScript/data-files/Chapter01/js01/project04/project01-04_txt.js
@@ -2,11 +2,19 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Mason_Sutton-Schaub
+    Date:   10/28/2025
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
 
+let homeStreet = "1 Main St.";
+let homeCity = "Sicilia";
+let homeState = "MA";
+let homeCode = "02103";
+let workStreet = "15 Oak Ln.";
+let workCity = "Central City";
+let workState = "MA";
+let workCode = "02104";
