3a36ac88e6c5c4caff3667080446bfaf56ae67cd
Mason-Suttonschaub <Mason_Suttonschaub@insideranken.org>
2025-10-29 11:59:26 -0500

    proj01-04 and part of proj02-01

--- JavaScript/data-files/Chapter02/js02/project01/project02-01_txt.js
+++ JavaScript/data-files/Chapter02/js02/project01/project02-01_txt.js
@@ -3,9 +3,17 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Mason_Sutton-Schaub
+      Date:   10/29/2025
 
       Filename: project02-01.js
  */
+
+function FahrenheitToCelsius(degree){
+      return ((degree-32) / 1.8)
+}
+
+function CelsiusToFahrenheit(degree){
+      return (1.8 * degree + 32)
+}
 
