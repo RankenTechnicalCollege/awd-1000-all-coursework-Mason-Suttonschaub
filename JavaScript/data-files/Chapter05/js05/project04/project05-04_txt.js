0238ee2fde70295554c23ac309f4059e327e4f2f
Mason-Suttonschaub <Mason_Suttonschaub@insideranken.org>
2025-11-20 11:49:08 -0600

    worked on proj05-04

--- JavaScript/data-files/Chapter05/js05/project04/project05-04_txt.js
+++ JavaScript/data-files/Chapter05/js05/project04/project05-04_txt.js
@@ -4,8 +4,8 @@
       Project 05-04
 
       Project to display footnotes in a popup window
-      Author: 
-      Date:   
+      Author: Mason_Sutton-Schaub
+      Date:   11/20/2025
 
       Filename: project05-04.js
 */
@@ -13,3 +13,14 @@
 // Node list of phrases that are associated with footnotes
 let phrases = document.querySelectorAll("article blockquote dfn");
 
+for (let i = 0; i < phrases.length; i++){
+      phrases[i].addEventListener("onclick", function() {
+            let phrase = document.createElement('h1');
+            phrase.textContent = this.textContent;
+            let footnote = document.createElement('p');
+            footnote.textContent = footnotes[i];
+            footnote.style = "font-style: italic; font-size: 1.2em;";
+            let closeButton = document.createElement('input');
+            
+      })
+}
