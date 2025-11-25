a3a0e7d16a598c1e5a8a8a54c49fd912d282553e
Mason-Suttonschaub <Mason_Suttonschaub@insideranken.org>
2025-11-25 11:50:09 -0600

    proj06-01 started on proj06-02

--- JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
+++ JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
@@ -10,3 +10,18 @@
       Filename: project06-01.js
 */
 
+    let submitButton = document.getElementById("submitButton");
+    let pwd = document.getElementById("pwd");
+    let pwd2 = document.getElementById("pwd2");
+
+ 
+    submitButton.addEventListener("click", function() {
+
+    if (!pwd.checkValidity()) {
+    pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+    } else if (pwd.value !== pwd2.value) {
+    pwd2.setCustomValidity("Your passwords must match");
+    } else {
+    pwd.setCustomValidity("");
+    }
+    });
--- JavaScript/data-files/Chapter06/js06/project02/project06-02_txt.js
+++ JavaScript/data-files/Chapter06/js06/project02/project06-02_txt.js
@@ -4,10 +4,17 @@
       Project 06-02
 
       Project to turn a selection list into a selection of hypertext links
-      Author: 
-      Date:   
+      Author: Mason_Sutton-Schaub
+      Date:   11/25/2025
 
       Filename: project06-02.js
 */
 
 
+window.onload = function(){
+let allSelect = document.querySelectorAll("form#govLinks select");
+
+for (let i = 0; i < allSelect[i]; i++){
+      
+}
+}
